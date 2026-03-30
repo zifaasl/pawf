006be3ea59e5b3f30e784c4a1cb66ba9787037e1
zifaasl <difanazf06@gmail.com>
2026-03-30 15:05:03 +0700

    import v_portfolio

--- project2/app/Controllers/Home.php
+++ project2/app/Controllers/Home.php
@@ -4,8 +4,8 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        return view('v_portfolio'); # mengarahkan ke file v_portfolio.php yang ada di folder Views
     }
 }
