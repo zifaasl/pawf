f3735657dd0e033638005dbcf67f9fc24d6593ba
zifaasl <difanazf06@gmail.com>
2026-04-14 16:35:04 +0700

    Update project blog

--- project3/app/Config/Routes.php
+++ project3/app/Config/Routes.php
@@ -9,5 +9,14 @@ $routes->get('/', 'Home::index');
 $routes->get('/about', 'Page::about');
 $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
+
 $routes->get('/post', 'Post::index');
 $routes->get('/post/(:any)', 'Post::viewPost/$1');
+
+// $routes->group('admin', function($routes){
+// 	$routes->get('post', 'PostAdmin::index', ['filter' => 'login']);
+// 	$routes->get('post/(:segment)/preview', 'PostAdmin::preview/$1');
+// 	$routes->add('post/new', 'PostAdmin::create');
+// 	$routes->add('post/(:segment)/edit', 'PostAdmin::edit/$1');
+// 	$routes->get('post/(:segment)/delete', 'PostAdmin::delete/$1');
+// });
